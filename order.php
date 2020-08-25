9837a751d9e72452f4efb2884e31a5f1a15ea6ea
yogitasangwan <42772734+yogitasangwan@users.noreply.github.com>
2020-08-25 10:13:31 +0530

    Add files via upload

---
+++ order.php
@@ -0,0 +1,7 @@
+﻿
+
+<?php
+$order  = $client->order->create([  'receipt'         => 'order_rcptid_11',  'amount'          => 50000, // amount in the smallest currency unit  'currency'        => 'INR',// <a href="/docs/payment-gateway/payments/international-payments/#supported-currencies" target="_blank">See the list of supported currencies</a>.)  'payment_capture' =>  '0']);
+
+
+?>
